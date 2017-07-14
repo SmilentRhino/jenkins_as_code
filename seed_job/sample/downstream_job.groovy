f83dfb44d6d249b31f40e31561d5b2a6d82ec873
smilentrhino <smilentrhino@163.com>
2017-07-14 01:29:08 +0000

    Remove extra parameter

--- seed_job/sample/downstream_job.groovy
+++ seed_job/sample/downstream_job.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 node('master'){
     stage("Print") {
-        echo "STRING_PARAM: ${STRING_PARAM}" 
         build job: 'string_parameter_job', parameters: [string(name: 'STRING_PARAM', value: 'default_value')]
     }
 }
