2322f9d1b6214c394a9154d9eb6388178ee298d7
smilentrhino <smilentrhino@163.com>
2018-03-12 07:28:09 +0000

    Update plugin list and installation job

--- seed_job/jenkins_admin/install_plugins.groovy
+++ seed_job/jenkins_admin/install_plugins.groovy
@@ -1,9 +1,14 @@
+import jenkins.*
 import jenkins.model.*
+import hudson.*
+import hudson.model.*
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
-println ("$Workspace")
-//def inputFile = new File('.//plugin.json')
-//def plugin_list = new JsonSlurper().parse(inputFile)
+def plugin_list_path = build.workspace.toString() + '/seed_job/files/plugins.json'
+println ("$plugin_list_path")
+def inputFile = new File("$plugin_list_path")
+def plugin_list = new JsonSlurper().parse(inputFile)
 //def instance = Jenkins.getInstance()
-//println (plugin_list)
+println (plugin_list)
+
