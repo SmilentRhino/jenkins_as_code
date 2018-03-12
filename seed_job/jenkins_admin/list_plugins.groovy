924b7afdf7a448654aea1a24d0856965cff4747d
smilentrhino <smilentrhino@163.com>
2018-03-12 06:24:17 +0000

    Update list plugin

--- seed_job/jenkins_admin/list_plugins.groovy
+++ seed_job/jenkins_admin/list_plugins.groovy
@@ -1,4 +1,15 @@
-Jenkins.instance.pluginManager.plugins.each{
+import jenkins.model.*
+def instance = Jenkins.getInstance()
+instance.pluginManager.plugins.each{
   plugin -> 
     println ("${plugin.getDisplayName()} (${plugin.getShortName()}): ${plugin.getVersion()}")
+}
+
+println ('Split line')
+println ('-----------------------------------------------------')
+println ('-----------------------------------------------------')
+
+instance.pluginManager.plugins.each{
+  plugin -> 
+      println ("${plugin.getShortName()}: ${plugin.getVersion()}")
 }
