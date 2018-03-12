ce16a8ebb2fe295b18e121a1b5f3491ba9b5e00b
smilentrhino <smilentrhino@163.com>
2018-03-12 05:38:52 +0000

    Update readyaml sample

--- seed_job/sample/readyaml_example.groovy
+++ seed_job/sample/readyaml_example.groovy
@@ -1,13 +1,9 @@
 #!/usr/bin/env groovy
 node('master'){
-    scm {
-        git {
-            remote {
-                url('https://github.com/SmilentRhino/jenkins_as_code.git')
-            }
-            branch('feature/refactor')
-        }
+    stage('CheckOut'){
+        checkout([$class: 'GitSCM', branches: [[name: 'feature/refactor']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/SmilentRhino/jenkins_as_code.git']]])
     }
+
     stage("Print") {
         datas = readYaml file: 'seed_job/files/users.yml'
         echo "HELLO WORLD" 
