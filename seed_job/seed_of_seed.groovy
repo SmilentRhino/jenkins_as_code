1723ff6f59d3408bf05847b119cf27cf7bbf9a20
smilentrhino <smilentrhino@163.com>
2018-03-12 04:01:29 +0000

    fix path problem

--- seed_job/seed_of_seed.groovy
+++ seed_job/seed_of_seed.groovy
@@ -1,7 +1,7 @@
 job('seed_dsl') {
     steps {
         dsl {
-            text(readFileFromWorkspace('seed_job.groovy'))
+            text(readFileFromWorkspace('seed_job/seed_job.groovy'))
             removeAction('DELETE')
         }
     }
