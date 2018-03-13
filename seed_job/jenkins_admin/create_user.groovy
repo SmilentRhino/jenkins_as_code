771414447df17f0d120b2b122cd71ab0ae0f5193
smilentrhino <smilentrhino@163.com>
2018-03-13 03:50:02 +0000

    Update create user function, and users definition

--- seed_job/jenkins_admin/create_user.groovy
+++ seed_job/jenkins_admin/create_user.groovy
@@ -6,17 +6,51 @@ import hudson.model.*
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
+//Get users.json 
 def user_list_path = build.workspace.toString() + '/seed_job/files/users.json'
 println ("$user_list_path")
 def inputFile = new File("$user_list_path")
 def user_list = new JsonSlurper().parse(inputFile)
 println (user_list)
+
+//Get current users
 def instance = Jenkins.getInstance()
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+def allUsers = hudsonRealm.getAllUsers()
+def userIdList = []
+allUsers.each{ user->
+    userIdList.add(user.getId())
+}
+println ("$userIdList")
+println ("$allUsers")
+
+//Check if user property has changed
+boolean CheckChanges() {
+    //Not implement yet
+}
+
+//Ensure user present or absent
 user_list.each{ user->
-    println ("Ensure ${user.name} ...")
-    hudsonRealm.createAccount("$user.name","$user.pass")
+    if (user.name in userIdList) {
+        if (user.state == 'present') {
+            println ("$user.name already exists, skip creating...")
+            CheckChanges()
+        }
+        else{
+            println ("Removing $user.name")
+            hudsonRealm.getUser($user.name).delete()
+        }
+    } else{
+        if (user.state == 'present') {
+            println ("Creating user $user.name...")
+            hudsonRealm.createAccount("$user.name","$user.pass")
+        }
+        else{
+            //Doing nothing
+        }
+    }
 }
 instance.setSecurityRealm(hudsonRealm)
 instance.save()
+
 
