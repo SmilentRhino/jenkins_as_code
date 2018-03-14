e85417ce31b98e7076d1f8f4084c8f4e1e80f215
smilentrhino <smilentrhino@163.com>
2018-03-14 08:50:46 +0000

    Update list plugin
    update list plugin output
    update plugins yaml from new output

--- seed_job/jenkins_admin/list_plugins.groovy
+++ seed_job/jenkins_admin/list_plugins.groovy
@@ -1,7 +1,7 @@
 import jenkins.model.*
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
-def plugin_list = []
+def plugin_list = [:]
 def instance = Jenkins.getInstance()
 println ('Detail')
 println ('-----------------------------------------------------')
@@ -9,7 +9,7 @@ println ('-----------------------------------------------------')
 
 instance.pluginManager.plugins.each { plugin -> 
     println ("${plugin.getDisplayName()} (${plugin.getShortName()}): ${plugin.getVersion()}");
-    plugin_list.add(["${plugin.getShortName()}" : "${plugin.getVersion()}"]);
+    plugin_list["${plugin.getShortName()}"]  = "${plugin.getVersion()}"
 }
 println (plugin_list)
 println ('-----------------------------------------------------')
@@ -18,9 +18,8 @@ println ('Yaml')
 println ('-----------------------------------------------------')
 println ('-----------------------------------------------------')
 
-plugin_list.each{
-  plugin -> 
-  println ("${plugin[0]}:${plugin[1]}")
+plugin_list.each{name, version -> 
+  println ("$name : \"$version\"")
 }
 println ('-----------------------------------------------------')
 println ('-----------------------------------------------------')
@@ -28,3 +27,4 @@ println ('Json')
 println ('-----------------------------------------------------')
 println ('-----------------------------------------------------')
 println JsonOutput.prettyPrint(JsonOutput.toJson(plugin_list))
+
